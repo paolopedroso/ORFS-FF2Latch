a2e179dc5f2114915c10921ab49873c5123bb1ae
Paolo Pedroso <108847100+paolopedroso@users.noreply.github.com>
2026-03-06 17:56:29 -0800

    clkgate frontend

--- flow/platforms/sky130hd/dff2latch.v
+++ flow/platforms/sky130hd/dff2latch.v
@@ -45,3 +45,63 @@ module \$_DFF_PP0_
     );
 
 endmodule
+
+module \$_DLATCH_PP0_
+(
+    input D,
+    input E,
+    input R,
+    output Q
+);
+
+    wire inv_reset;
+    sky130_fd_sc_hd__inv_1 INV (
+        .A(R),
+        .Y(inv_reset)
+    );
+
+    sky130_fd_sc_hd__dlrtp_1 _TECHMAP_REPLACE_ (
+        .GATE(E),
+        .RESET_B(inv_reset),
+        .D(D),
+        .Q(Q)
+    );
+
+endmodule
+
+
+module \$_DLATCH_P_
+(
+    input D,
+    input E,
+    output Q
+);
+
+    // dlxtp only has 1 size
+    sky130_fd_sc_hd__dlxtp_1 _TECHMAP_REPLACE_
+    (
+        .D(D),
+        .GATE(E),
+        .Q(Q)
+    );
+
+endmodule
+
+// From https://github.com/YosysHQ/yosys/blob/main/techlibs/common/simcells.v:
+// "A positive edge D-type flip-flop with negative polarity reset."
+module \$_DFF_PN0_
+(
+    input D,
+    input C,
+    input R,
+    output Q
+);
+
+    sky130_fd_sc_hd__dlrtp_1 _TECHMAP_REPLACE_ (
+        .GATE(C),
+        .RESET_B(R),
+        .D(D),
+        .Q(Q)
+    );
+
+endmodule
---
+++ flow/platforms/sky130hd/duplicate_DFFs_clkgate.v
@@ -0,0 +1,434 @@
+// This file duplicates each DFF, and also adds extra flip-flops to make sure that each enable and
+// reset are clocked by the correct clock (i.e., clock 1 goes to clock 2, clock 2 goes to clock 1).
+
+module \$_DFF_P_ (input D, C, output Q);
+    wire connector;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_2 
+    (.D(connector), .C(C), .Q(Q));
+endmodule
+
+module \$_DFFE_PP_ (input D, C, E, output Q);
+    wire connector;
+    wire enable_output;
+    wire gated_clk;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en 
+    (.A(enable_output), .B(C), .X(gated_clk));
+
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2
+    (.D(connector), .C(gated_clk), .Q(Q));
+endmodule
+
+module $_SDFFE_PP0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire or_output;
+    wire gated_clk;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    sky130_fd_sc_hd__or2_1 OR_gate_clk 
+    (.A(enable_output), .B(reset_output), .X(or_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(or_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(connector), .B(1'b0), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_SDFFE_PN0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire inv_reset_output;
+    wire or_output;
+    wire gated_clk;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    \$_NOT_ INV_rst 
+    (.A(reset_output), .Y(inv_reset_output));
+    
+    sky130_fd_sc_hd__or2_1 OR_gate_clk 
+    (.A(inv_reset_output), .B(enable_output), .X(or_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(or_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(1'b0), .B(connector), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_DFFE_PP0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire gated_clk;
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .R(R), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(enable_output), .B(C), .X(gated_clk));
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(connector), .C(gated_clk), .R(reset_output), .Q(Q));
+endmodule
+
+module \$_SDFF_PP0_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+    wire gated_clk;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(reset_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(1'b0), .B(connector), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_SDFF_PP1_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_MUX_ mux_set_inst 
+    (.Y(mux_output), .A(connector), .B(1'b1), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_2 
+    (.D(mux_output), .C(C), .Q(Q));
+endmodule
+
+module \$_DFF_PP0_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_FF_output;
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_1 (.D(D), .C(C), .R(R), .Q(connector));
+
+    \$_DFF_P_ reset_FF  (.D(R), .C(C), .Q(reset_FF_output));
+
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_2 (.D(connector), .C(C), .R(reset_FF_output), .Q(Q));
+endmodule
+
+module \$_DFF_PN0_ (input D, C, R, output Q);
+    wire connector;
+    wire inv_reset_output;
+
+    \$_NOT_ INV_rst
+    (.A(R), .Y(inv_reset_output));
+
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_1 (.D(D), .C(C), .R(inv_reset_output), .Q(connector));
+
+    wire reset_FF_output;
+    \$_DFF_P_ reset_FF  (.D(inv_reset_output), .C(C), .Q(reset_FF_output));
+
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_2 (.D(connector), .C(C), .R(reset_FF_output), .Q(Q));
+endmodule
+
+//////////////////////////////////////// added modules ////////////////////////////////////////
+
+module \$_SDFF_PN1_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_MUX_ mux_set_inst 
+    (.Y(mux_output), .A(1'b1), .B(connector), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_2 
+    (.D(mux_output), .C(C), .Q(Q));
+endmodule
+
+module \$_SDFF_PN0_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(1'b0), .B(connector), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_2 
+    (.D(mux_output), .C(C), .Q(Q));
+
+endmodule
+
+module \$_SDFFCE_PP0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire gated_clk;
+    wire gated_rst;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+
+    sky130_fd_sc_hd__and2_1 AND_gate_rst
+    (.A(reset_output), .B(C), .X(gated_rst));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(enable_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(connector), .B(1'b0), .S(gated_rst));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_SDFFCE_PP1P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire gated_clk;
+    wire gated_rst;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+
+    sky130_fd_sc_hd__and2_1 AND_gate_rst
+    (.A(reset_output), .B(C), .X(gated_rst));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(enable_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(connector), .B(1'b1), .S(gated_rst));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_DFF_PN1_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+    wire mux_output;
+    wire inv_reset_output;
+    wire or_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+
+    \$_MUX_ mux_set_inst 
+    (.Y(mux_output), .A(1'b1), .B(connector), .S(reset_output));
+
+    \$_NOT_ INV_rst 
+    (.A(R), .Y(inv_reset_output));
+    
+    sky130_fd_sc_hd__or2_1 OR_gate_clk.gated_en
+    (.A(inv_reset_output), .B(C), .X(or_output));
+        
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(mux_output), .C(or_output), .Q(Q));
+endmodule
+
+module \$_DFF_NN0_ (input D, C, R, output Q);
+    wire connector;
+    wire reset_output;
+
+    \$_DFF_P_ reset_FF
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_2
+    (.D(D), .C(C), .R(R), .Q(connector));
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_1
+    (.D(connector), .C(C), .R(reset_output), .Q(Q));
+endmodule
+
+module \$_DLATCH_N_ (input E, D, output Q);
+
+    wire connector;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_2 
+    (.D(D), .C(E), .Q(connector));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1
+    (.D(connector), .C(E), .Q(Q));
+
+endmodule
+
+module \$_DFFE_PN0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire inv_reset_output;
+    wire or_output;
+    wire gated_clk;
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .R(R), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    \$_NOT_ INV_rst 
+    (.A(reset_output), .Y(inv_reset_output));
+    
+    sky130_fd_sc_hd__or2_1 OR_gate_clk 
+    (.A(inv_reset_output), .B(enable_output), .X(or_output));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(or_output), .B(C), .X(gated_clk));
+    
+    \$_DFF_PP0_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(connector), .C(gated_clk), .R(reset_output), .Q(Q));
+endmodule
+
+module \$_SDFFCE_PN0P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire gated_clk;
+    wire gated_rst;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+
+    sky130_fd_sc_hd__and2_1 AND_gate_rst
+    (.A(reset_output), .B(C), .X(gated_rst));
+    
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(enable_output), .B(C), .X(gated_clk));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(1'b0), .B(connector), .S(gated_rst));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2
+    (.D(mux_output), .C(gated_clk), .Q(Q));
+endmodule
+
+module \$_DFFE_PN1P_ (input D, C, R, E, output Q);
+    wire connector;
+    wire reset_output;
+    wire enable_output;
+    wire inv_reset_output;
+    wire or_en_rst;
+    wire gated_clk_2_en;
+    wire gated_rst;
+    wire mux_output;
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_FF_replace_1 
+    (.D(D), .C(C), .Q(connector));
+    
+    \$_DFF_P_ reset_FF 
+    (.D(R), .C(C), .Q(reset_output));
+    
+    \$_DFF_P_ enable_FF 
+    (.D(E), .C(C), .Q(enable_output));
+    
+    \$_NOT_ INV_rst 
+    (.A(reset_output), .Y(inv_reset_output));
+
+    sky130_fd_sc_hd__or2_1 OR_gate_en_rst
+    (.A(enable_output), .B(inv_reset_output), .X(or_en_rst));
+
+    sky130_fd_sc_hd__and2_1 AND_gate_clk.gated_en
+    (.A(or_en_rst), .B(C), .X(gated_clk_2_en));
+
+    sky130_fd_sc_hd__or2_1 OR_gate_clk
+    (.A(gated_clk_2_en), .B(inv_reset_output), .X(gated_rst));
+    
+    \$_MUX_ mux_rst_inst 
+    (.Y(mux_output), .A(1'b1), .B(connector), .S(reset_output));
+    
+    \$_DFF_P_ _TECHMAP_REPLACE_.custom_gated_FF_2 
+    (.D(mux_output), .C(gated_rst), .Q(Q));
+endmodule
+
+    // TODO //
+//////////////////////////////////////// Needs Verification ////////////////////////////////////////
